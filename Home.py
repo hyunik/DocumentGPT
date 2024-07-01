4871e586f78e3c19848d0c828ca31594c67c896b
hyunik <47076464+hyunik@users.noreply.github.com>
2024-07-01 22:46:24 +0900

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from datetime import datetime
+
+st.set_page_config(
+    page_title="FUllstackGPT Home",
+    page_icon=":)"
+)
+
+st.title("FullstackGPT Home")
